5f021c8d16b09ad3632be68b42eae2bf24cbf468
lucaspdc21 <lucaspdc21@gmail.com>
2024-09-11 15:53:47 -0300

    geracao das telas principais

---
+++ src/main/java/com/java/gerador/Gerador_tabela_verdade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.java.gerador;
+
+/**
+ *
+ * @author lucaspdc21
+ */
+public class Gerador_tabela_verdade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
